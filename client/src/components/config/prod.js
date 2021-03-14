73dfa9826a2ae8ba43a714b4e45b9b629532ef23
danny <danielniedzwiedzki.1@gmail.com>
2021-03-14 19:09:42 -0400

    changes

--- client/src/components/config/prod.js
+++ client/src/components/config/prod.js
@@ -1,6 +1,6 @@
 
 
 module.exports={
-    mapboxApiAccessToken: process.env.MAPBOXAPIACCESSTOKEN,
-    test: process.env.TEST
+    mapboxApiAccessToken: process.env.REACT_APP_MAPBOXAPIACCESSTOKEN,
+    test: process.env.REACT_APP_TEST
 }
